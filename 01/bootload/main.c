59dfba7a1275738ae7b20ec189c985e8a2130159
Yohei Shimmyo <qwpmb554@gmail.com>
2019-06-11 21:47:27 +0900

    add memory and string libraries

--- 01/bootload/main.c
+++ 01/bootload/main.c
@@ -6,7 +6,8 @@
 int main(void)
 {
   serial_init(51);
-  puts((unsigned char *)"Hello World!\n");
+  // puts((unsigned char *)"Hello World!\n");
+  putxval(0x10, 0); puts("\n");
   while(1);
   return 0;
 }
